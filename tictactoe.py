a30bfa0e001177cacdb42a1666c5d1717ad24ff4
kshitishsorte <kshitish.999.sorte@gmail.com>
2022-01-08 01:23:32 +0530

    second commit

--- tictactoe.py
+++ tictactoe.py
@@ -44,17 +44,14 @@ def place_marker(board, marker, position):
 
 
 def check_win(board, mark):
-    if ((board[0] == board[1] == board[2] == mark) or
+    return ((board[0] == board[1] == board[2] == mark) or
     (board[3] == board[4] == board[5] == mark) or
     (board[6] == board[7] == board[8] == mark) or
     (board[0] == board[3] == board[6] == mark) or
     (board[1] == board[4] == board[7] == mark) or
     (board[2] == board[5] == board[8] == mark) or
     (board[0] == board[4] == board[8] == mark) or
-    (board[2] == board[4] == board[6] == mark)):
-        print("True")
-    else:
-        print("False")
+    (board[2] == board[4] == board[6] == mark))
 
 
 check_win(test_board, 'X')
@@ -66,68 +63,21 @@ def choose_first():
         return 'Player 1'
 
 def space_chk(board, position):
-    
-    # while position_list[0] != position_list[1] != position_list[2] != 'X' or position_list[3] != position_list[4] != \
-    #         position_list[5] != 'X' or position_list[6] != position_list[7] != position_list[8] != 'X' or position_list[
-    #     0] != position_list[3] != position_list[6] != 'X' or position_list[1] != position_list[4] != position_list[
-    #     7] != 'X' or position_list[2] != position_list[5] != position_list[8] != 'X' or position_list[0] != \
-    #         position_list[4] != position_list[8] != 'X' or position_list[2] != position_list[4] != position_list[
-    #     6] != 'X' or position_list[0] != position_list[1] != position_list[2] != 'O' or position_list[3] != \
-    #         position_list[4] != position_list[5] != 'O' or position_list[6] != position_list[7] != position_list[
-    #     8] != 'O' or position_list[0] != position_list[3] != position_list[6] != 'O' or position_list[1] != \
-    #         position_list[4] != position_list[7] != 'O' or position_list[2] != position_list[5] != position_list[
-    #     8] != 'O' or position_list[0] != position_list[4] != position_list[8] != 'O' or position_list[2] != \
-    #         position_list[4] != position_list[6] != 'O':
-    #     pos = int(input("Enter the position "))
-    #     marker = choice
+    return board[position] == ' '
 
-    #     if pos == 1:
-    #         position_list[0] = marker
-    #     if pos == 2:
-    #         position_list[1] = marker
-    #     if pos == 3:
-    #         position_list[2] = marker
-    #     if pos == 4:
-    #         position_list[3] = marker
-    #     if pos == 5:
-    #         position_list[4] = marker
-    #     if pos == 6:
-    #         position_list[5] = marker
-    #     if pos == 7:
-    #         position_list[6] = marker
-    #     if pos == 8:
-    #         position_list[7] = marker
-    #     if pos == 9:
-    #         position_list[8] = marker
-    #     display_board(pos_list)
+def fullboard_check(board):
+    for i in range(0,9):
+        if space_chk(board, i):
+            return False
+    return True
 
-    # pos = int(input("Enter the position "))
-    # marker = choice
-    #
-    # if pos == 1:
-    #     position_list[0] = marker
-    # if pos == 2:
-    #     position_list[1] = marker
-    # if pos == 3:
-    #     position_list[2] = marker
-    # if pos == 4:
-    #     position_list[3] = marker
-    # if pos == 5:
-    #     position_list[4] = marker
-    # if pos == 6:
-    #     position_list[5] = marker
-    # if pos == 7:
-    #     position_list[6] = marker
-    # if pos == 8:
-    #     position_list[7] = marker
-    # if pos == 9:
-    #     position_list[8] = marker
-    # display_board(pos_list)
+def player_choice(board):
+    position=0
 
+    while position not in [0,1,2,3,4,5,6,7,8] or not space_chk(board , position):
+        position = int(input("Choose your next position: (0-8) "))
 
-#game_play(pos_list, choice)
+    return position
 
-
-
-
-
+def replay():
+    return input("hello")
